5ddcdbb6aec59c422d7f411e808a5316c94327d9
Bao Quan Lindgren <bao.lindgren@gmail.com>
2024-03-25 17:04:47 +0100

    chore: refactor to make module OO

--- src/util/model.py
+++ src/util/model.py
@@ -1,28 +1,86 @@
-import os
-from transformers import AutoTokenizer, AutoModelForCausalLM, PreTrainedTokenizer, PreTrainedTokenizerFast, PreTrainedModel
+from __future__ import annotations
+from os import PathLike
+from transformers import AutoTokenizer, AutoModelForCausalLM, Conversation, PreTrainedTokenizer, PreTrainedTokenizerFast, PreTrainedModel
 import torch
 from typing import Final
-from model import *
+from .model import *
 
 
-# ModelData: TypeAlias = tuple[PreTrainedTokenizer | PreTrainedTokenizerFast, PreTrainedModel]
-_MODELS: Final[dict[str | os.PathLike, ModelData]] = {}
+class Model:
+    _MODELS: Final[dict[str | PathLike, Model]] = {}
 
+    # Instruction suffix
+    _INST_SUFFIX: Final[str] = "[/INST]"
+    _INST_SUFFIX_LEN : Final[int] = len(_INST_SUFFIX)
 
-def load_model(model_name_or_path: str | os.PathLike) -> ModelData:
-    model_data: ModelData = _MODELS.get(model_name_or_path, None)
+    # System prompt used for REST-at
+    _SYSTEM_PROMPT: Final[str] = "You are a helpful AI called Kalle."
 
-    # Return if the specified model has already been loaded
-    if model_data:
-        return model_data
+    @staticmethod
+    def load(model_name_or_path: str | PathLike, max_new_tokens: int) -> Model:
+        m: Model = Model.get(model_name_or_path)
 
-    # Load the model and its tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-    model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float16, device_map="auto")
+        # Return model if already loaded
+        if m:
+            return m
 
-    _MODELS[model_name_or_path] = (tokenizer, model)
+        # Load the model and its tokenizer
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float16, device_map="auto")
 
-    return tokenizer, model
+        Model._MODELS[model_name_or_path] = m = Model(tokenizer, model, max_new_tokens)
 
-def get_model(model_name_or_path: str | os.PathLike) -> ModelData | None:
-    return _MODELS.get(model_name_or_path)
+        return m
+
+    @staticmethod
+    def get(model_name_or_path: str | PathLike):
+        return Model._MODELS.get(model_name_or_path, None)
+    
+    @staticmethod
+    def _gen_prompt(user_prompt: str) -> str:
+        """
+        Generate a prompt using a predefined system prompt.
+
+        Parameters:
+        -----------
+        `user_prompt: str` - The user prompt. Must consist of at least 1 non-whitespace character.
+
+        Returns:
+        --------
+        `str` - A formatted prompt.
+
+        Raises:
+        -------
+        `ValueError` if `user_prompt` is empty, `None`, or consists of only whitespace characters.
+        """
+        prompt: str = user_prompt.strip()
+
+        if not prompt:
+            raise ValueError("User prompt must consist of at least 1 non-whitespace character.")
+
+        return f"[SYS] {Model._SYSTEM_PROMPT} [/SYS]\n\n{user_prompt}"
+
+    def __init__(self, tokenzer: PreTrainedTokenizer | PreTrainedTokenizerFast, model: PreTrainedModel, max_new_tokens: int):
+        self.tokenzer: PreTrainedTokenizer | PreTrainedTokenizerFast = tokenzer
+        self.model: PreTrainedModel = model
+        self.max_new_tokens: int = max_new_tokens
+
+    def prompt(self, history: list[dict[str, str]] | Conversation, prompt: str) -> str:
+        history.append({"role": "user", "content": Model._gen_prompt(prompt)})
+        input_ids: str | list[int] = self.tokenizer.apply_chat_template(history, return_tensors="pt").to("cuda")
+
+        outputs = self.model.generate(
+            input_ids,
+            max_new_tokens=self.max_new_tokens,
+            do_sample=True,
+            temperature=0.1
+        )
+
+        raw_res: str = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        # Cut out the instruction section of the output
+        res: str = raw_res[(raw_res.find(Model._INST_SUFFIX) + Model._INST_SUFFIX_LEN)::].strip()
+
+        # Append response to history
+        history.append({"role": "assistant", "content": res})
+        return res
--- src/util/model.pyi
+++ src/util/model.pyi
@@ -3,41 +3,68 @@ Utility module for handling models.
 
 Includes:
 ---------
-`load_model` - Loads a model.
-`get_model` - Gets a model if loaded.
+`Model` - A class abstracting pretrained models, providing methods to load, retrieve, and prompt a model.
 """
-import os
-from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast, PreTrainedModel
-from typing import TypeAlias
+from os import PathLike
+from transformers import Conversation
 
 
-ModelData: TypeAlias = tuple[PreTrainedTokenizer | PreTrainedTokenizerFast, PreTrainedModel]
-
-
-def load_model(model_name_or_path: str | os.PathLike) -> ModelData:
+class Model:
     """
-    Loads a specified model if not already loaded.
+    A class abstracting pretrained models, providing functions to load, retrieve, and prompt a model.
 
-    Parameters:
-    -----------
-    `model_name_or_path: str | PathLike` - The model to load. Can be either a model name from Hugging Face Hub or a path to a local model.
-
-    Returns:
+    Methods:
     --------
-    `tuple[PreTrainedTokenizer | PreTrainedTokenizerFast, PreTrainedModel]` The tokenizer and the model itself.
+    `static load -> Model` - Loads a given model if not already loaded and returns the model.\n
+    `static get -> Model` - Gets a loaded model or `None` if not loaded.\n
+    `prompt -> str` - Prompts the model and returns the response.
     """
-    pass
 
-def get_model(model_name_or_path: str | os.PathLike) -> ModelData | None:
-    """
-    Gets a model if loaded, else `None`
+    @staticmethod
+    def load(model_name_or_path: str | PathLike, max_new_tokens: int) -> Model:
+        """
+        Loads a specified model if not already loaded.
 
-    Parameters:
-    -----------
-    `model_name_or_path: str | PathLike` - The model to get. Can be either a model name from Hugging Face Hub or a path to a local model.
+        Parameters:
+        -----------
+        model_name_or_path: str | PathLike - The model to load. Can be either a model name from Hugging Face Hub or a path to a local model.
+        max_new_tokens: int - The `max_new_tokens` parameter used when generating.
+        Returns:
+        --------
+        `Model` The model that was loaded.
+        """
+        ...
 
-    Returns:
-    --------
-    `tuple[PreTrainedTokenizer | PreTrainedTokenizerFast, PreTrainedModel] | None` The tokenizer and the model itself if loaded, else None.
-    """
-    pass
+    @staticmethod
+    def get(model_name_or_path: str | PathLike) -> Model:
+        """
+        Gets a model if loaded, else `None`
+
+        Parameters:
+        -----------
+        model_name_or_path: str | PathLike - The model to get. Can be either a model name from Hugging Face Hub or a path to a local model.
+
+        Returns:
+        --------
+        `Model | None` The tokenizer and the model itself if loaded, else None.
+        """
+        ...
+
+    def prompt(self, history: list[dict[str, str]] | Conversation, prompt: str) -> str:
+        """
+        Prompts a the model and gets the response.
+
+        Parameters:
+        -----------
+        history: list[dict[str, str]] | Conversation - The conversation history. Must adhere to model constraints.
+        prompt: str - The user prompt to send to the model. Must
+
+        Returns:
+        --------
+        `str` - The response from the model if the model is loaded, else `None`.
+
+        Raises:
+        -------
+        `ValueError` if `user_prompt` is empty, `None`, or consists of only whitespace characters.
+        """
+        ...
