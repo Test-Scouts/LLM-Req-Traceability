5358342221259e829c2decf156468bb1fb3049e9
Bao Quan Lindgren <bao.lindgren@gmail.com>
2024-03-26 10:32:26 +0100

    refactor: add exit keyword to chat

--- src/examples/transformers_example.py
+++ src/examples/transformers_example.py
@@ -6,14 +6,19 @@ from ..util import model as ml
 def main() -> None:
     load_dotenv()
 
+    # Model data
     model_id: str = os.getenv("MODEL_PATH")
     max_new_tokens: int = 4096
 
+    # Load model
     model : ml.Model = ml.Model.load(model_id, max_new_tokens)
 
+    # Create an empty message history
     messages: list[dict[str, str]] = []
 
-    while True:
+    # Chat loop
+    user_prompt: str = ""
+    while user_prompt.lower() != "bye":
         user_prompt = input("> ")
         res = model.prompt(messages, user_prompt)
         print(f"\nLLM> {res}\n")
