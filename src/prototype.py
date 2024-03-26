f7d23b7ad5bca0030da0199b5acf8eb039c9f57a
Bao Quan Lindgren <bao.lindgren@gmail.com>
2024-03-26 15:31:48 +0100

    fix: fix send button logic

--- src/prototype.py
+++ src/prototype.py
@@ -69,16 +69,17 @@ def interact_with_model() -> None:
     user_prompt: str = input_container.text_input("Input", key="text-input")
 
     # Ignore the rest if the send button isn't clicked
-    if input_container.button("Send"):
-        # Disable Input field
-        input_field.empty()
-        input_container = input_field.container()
-        user_prompt: str = input_container.text_input("Input", key="text-input-disabled", disabled=True)
-        input_container.button("Send", key="send-disabled", disabled=True)
-
-        # Render new input
-        history.write(f"### User\n{user_prompt}")
+    if not input_container.button("Send"):
         return
+
+    # Disable Input field
+    input_field.empty()
+    input_container = input_field.container()
+    input_container.text_input("Input", key="text-input-disabled", disabled=True)
+    input_container.button("Send", key="send-disabled", disabled=True)
+
+    # Render new input
+    history.write(f"### User\n{user_prompt}")
 
     # Clear message history if wanted
     if user_prompt.lower() == "clear":
