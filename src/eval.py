97f22b8e10e116bbace6d28882193f38f4ac910d
Bao Quan Lindgren <bao.lindgren@gmail.com>
2024-04-05 16:02:05 +0200

    enhance: evaluate outputs in batches

--- src/eval.py
+++ src/eval.py
@@ -8,21 +8,15 @@ from dotenv import load_dotenv
 def main() -> None:
     load_dotenv()
 
-    out_path: str = os.getenv("OUT_PATH")
-
-    # Load the tool output
-    res: list[dict[str, str]]
-    with open(out_path, "r") as f:
-        res = json.load(f)
-
     # Load the set of tests
     tests: set[str]
     with open(os.getenv("TEST_PATH"), "r") as f:
         reader: csv.DictReader = csv.DictReader(f)
-        tests = set(map(lambda row: row["ID"], reader))
+        tests = {row["ID"] for row in reader}
     
+    # Maps Req ID -> Test IDs
+    map_: dict[str, list[str]]
     # Load the mappings
-    map_: list[dict[str, str | list[str]]]
     with open(os.getenv("MAP_PATH"), "r") as f:
         fields: list[str] = [
             "Req ID",
@@ -30,65 +24,82 @@ def main() -> None:
         ]
         reader: csv.DictReader = csv.DictReader(f)
 
-        map_ = list(map(lambda row: {k: row[k] for k in row.keys() if k in fields}, reader))
+        # {"Req ID": <Req ID>, "Test IDs": <Test IDs>} for each row
+        tmp: list[dict[str, str | list[str]]] = [
+            {k: row[k] for k in row.keys() if k in fields}
+            for row in reader
+        ]
 
-        for e in map_:
+        for e in tmp:
             e["Test IDs"] = e["Test IDs"].replace(" ", "").split(",") if e["Test IDs"] else []
 
-    # Values for confusion matrix
-    n: int = 0
-    tp: int = 0
-    tn: int = 0
-    fp: int = 0
-    fn: int = 0
+        map_ = {e["Req ID"]: e["Test IDs"] for e in tmp}
 
-    for req in res:
-        req_id: str = req["requirementID"]
-        actual_tests: set[str] = set(req["tests"].replace(" ", "").split(","))
+    # Evaluate results of every output
+    for model in os.listdir(f"./out"):
+        for day in os.listdir(f"./out/{model}"):
+            for time in os.listdir(f"./out/{model}/{day}"):
+                # Skip outputs that already have been evaluated to save time and compute
+                if "eval.log" in os.listdir(f"./out/{model}/{day}/{time}"):
+                    continue
 
-        expected_tests: set[str]
-        try:
-            expected_tests = set([e["Test IDs"] for e in map_ if e["Req ID"] == req_id][0])
-        except:
-            expected_tests = set()
+                out_path: str = f"./out/{model}/{day}/{time}/res.json"
 
-        tps: set[str] = actual_tests & expected_tests
-        tpsn: int = len(tps)
-        fps: set[str] = actual_tests - expected_tests
-        fpsn: int = len(fps)
+                # Load the tool output
+                res: list[dict[str, str]]
+                with open(out_path, "r") as f:
+                    res = json.load(f)
 
-        # Negatives
-        expected_ns: set[str] = tests - expected_tests
-        actual_ns: set[str] = tests - actual_tests
+                # Values for confusion matrix
+                n: int = 0
+                tp: int = 0
+                tn: int = 0
+                fp: int = 0
+                fn: int = 0
 
-        tns: set[str] = actual_ns & expected_ns
-        tnsn: int = len(tns)
-        fns: set[str] = actual_ns - expected_ns
-        fnsn: int = len(fns)
+                for req in res:
+                    req_id: str = req["requirementID"]
+                    actual_tests: set[str] = set(req["tests"].replace(" ", "").split(","))
 
-        n += tpsn + fpsn + tnsn + fnsn
-        tp += tpsn
-        tn += tnsn
-        fp += fpsn
-        fn += fnsn
-    
-    accuracy: float = 100 * (tp + tn) / n if n != 0 else 0
-    recall: float = 100 * tp / (tp + fn) if tp + fn != 0 else 0
-    precision: float = 100 * tp / (tp + fp) if tp + fp != 0 else 0
+                    expected_tests: set[str] = set(map_.get(req_id, []))
 
-    eval_path = f"{os.path.dirname(out_path)}/eval.log"
-    lines: list[str] = [
-        f"{n=}",
-        f"{tp=}",
-        f"{tn=}",
-        f"{fp=}",
-        f"{fn=}",
-        f"{accuracy=}%",
-        f"{recall=}%",
-        f"{precision=}%"
-    ]
-    with open(eval_path, "w+") as f:
-        f.write("\n".join(lines) + "\n")
+                    tps: set[str] = actual_tests & expected_tests
+                    tpsn: int = len(tps)
+                    fps: set[str] = actual_tests - expected_tests
+                    fpsn: int = len(fps)
+
+                    # Negatives
+                    expected_ns: set[str] = tests - expected_tests
+                    actual_ns: set[str] = tests - actual_tests
+
+                    tns: set[str] = actual_ns & expected_ns
+                    tnsn: int = len(tns)
+                    fns: set[str] = actual_ns - expected_ns
+                    fnsn: int = len(fns)
+
+                    n += tpsn + fpsn + tnsn + fnsn
+                    tp += tpsn
+                    tn += tnsn
+                    fp += fpsn
+                    fn += fnsn
+                
+                accuracy: float = 100 * (tp + tn) / n if n != 0 else 0
+                recall: float = 100 * tp / (tp + fn) if tp + fn != 0 else 0
+                precision: float = 100 * tp / (tp + fp) if tp + fp != 0 else 0
+
+                eval_path = f"{os.path.dirname(out_path)}/eval.log"
+                lines: list[str] = [
+                    f"{n=}",
+                    f"{tp=}",
+                    f"{tn=}",
+                    f"{fp=}",
+                    f"{fn=}",
+                    f"{accuracy=}%",
+                    f"{recall=}%",
+                    f"{precision=}%"
+                ]
+                with open(eval_path, "w+") as f:
+                    f.write("\n".join(lines) + "\n")
 
 
 if __name__ == "__main__":
