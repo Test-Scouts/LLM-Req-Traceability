c59e6a8ebedda89ec79c4d9ab6ae6a27192a5397
Nicole Quinstedt <gusquini@student.gu.se>
2024-04-16 14:34:22 +0200

    add req and st to test

--- translator/ai-swe-translator.py
+++ translator/ai-swe-translator.py
@@ -25,23 +25,46 @@ pipe = pipeline(
 )
 
 stop_on_token_criteria = StopOnTokenCriteria(stop_token_id=pipe.tokenizer.bos_token_id)
-text = "Jag har krav att översätta till Engelska som är för ett projekt"
 
-# This will translate English to Swedish
-# To translate from Swedish to English the prompt would be:
-prompt = f"<|endoftext|><s>User: Översätt till Engelska från Svenska\n{text}<s>Bot:"
+def translate_to_swedish(text):
+    # This will translate English to Swedish
+    # To translate from Swedish to English the prompt would be:
+    prompt = f"<|endoftext|><s>User: Översätt till Engelska från Svenska\n{text}<s>Bot:"
 
-#prompt = f"<|endoftext|><s>User: Översätt till Svenska från Engelska\n{text}<s>Bot:"
+    #prompt = f"<|endoftext|><s>User: Översätt till Svenska från Engelska\n{text}<s>Bot:"
 
-input_tokens = pipe.tokenizer(prompt, return_tensors="pt").input_ids.to(device)
-max_model_length = 2048
-dynamic_max_length = max_model_length - input_tokens.shape[1]
+    input_tokens = pipe.tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+    max_model_length = 2048
+    dynamic_max_length = max_model_length - input_tokens.shape[1]
 
-response = pipe(
-    prompt,
-    max_length=dynamic_max_length,
-    truncation=True,
-    stopping_criteria=StoppingCriteriaList([stop_on_token_criteria])
-)
+    response = pipe(
+        prompt,
+        max_length=dynamic_max_length,
+        truncation=True,
+        stopping_criteria=StoppingCriteriaList([stop_on_token_criteria])
+    )
 
-print(response[0]["generated_text"].split("<s>Bot: ")[-1])
+    return response[0]["generated_text"].split("<s>Bot: ")[-1]
+
+
+
+requirement = """
+S257 - Ny nyckel/lösnenord
+
+Fabriksinställningar och leverantörsspecifika standardlösenord eller krypteringsnycklar ska (S257) ersättas med nyskapade motsvarigheter som uppfyller Beställarens krav.
+"""
+test_case = """
+S257 - Ny nyckel/lösenord
+
+Exporta nyckel för mätaren från KMS.
+Koppla upp mot mätaren med den exporterade nyckeln med MeterTool.
+Byt nyckel för mätaren i KMS.
+Exportera nycklar för mätaren igen från KMS.
+Koppla upp mot mätaren med MeterTool igen med de gamla kryptonycklarna.
+Importera nya nycklarna i MeterTool.
+Koppla upp mot mätaren med MeterTool igen med de nya kryptonycklarna.
+
+"""
+
+sp="\n\n"
+print(translate_to_swedish(requirement)+sp)
