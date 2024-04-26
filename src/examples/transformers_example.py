4024528d791bb7cac7846baf444f1e569b979de2
Bao Quan Lindgren <bao.lindgren@gmail.com>
2024-04-26 16:10:04 +0200

    wip: add test file for llama 3

---
+++ src/examples/transformers_example.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BatchEncoding,
+    PreTrainedTokenizer,
+    PreTrainedTokenizerFast,
+    PreTrainedModel
+)
+import torch
+
+
+def main() -> None:
+    load_dotenv()
+
+    # Model data
+    model_id: str = os.getenv("MODEL_PATH")
+    max_new_tokens: int = int(os.getenv("TOKEN_LIMIT"))
+
+    # Load model# Load the model and its tokenizer
+    tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.chat_template
+
+    model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        torch_dtype=torch.float16,
+        device_map="auto"
+    )
+    model.eval()
+
+    # Create an empty message history
+    messages: list[dict[str, str]] = []
+
+    user_prompt = input("> ")
+    messages.append(user_prompt)
+
+    input_ids: BatchEncoding = tokenizer.apply_chat_template(user_prompt)
+
+    outputs = model.generate(
+        **input_ids,
+        max_new_tokens=max_new_tokens,
+        do_sample=True,
+        temperature=0.1
+    )
+    res = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(f"\nLLM> {res}\n")
+
+
+if __name__ == "__main__":
+    main()
