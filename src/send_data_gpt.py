98046e43544ea89775fb0dfab7f8d46fa7c30596
Bao Quan Lindgren <bao.lindgren@gmail.com>
2024-04-29 16:53:31 +0200

    refactor: change default model to GPT-3.5

--- src/send_data_gpt.py
+++ src/send_data_gpt.py
@@ -7,34 +7,37 @@ from dotenv import load_dotenv
 
 from .core.rest import RESTSpecification
 
-parser = argparse.ArgumentParser(description="Process file information.")
-parser.add_argument("--sessionName", "-s", dest="session",type=str, default= "MistralAI-REST-at-BTHS-eval", help="Customize the session name")
-parser.add_argument("--model", "-m", dest="model",type=str, default= "gpt4", help="Set the model to use. Choose between GPT3.5 and GPT4. Default is GPT4.")
-parser.add_argument("--data", "-d", dest="data",type=str, default= "GBG", help="Customize the dataset, not case sensitive. Use MIX for the mix dataset, BTHS for the BTHS dataset, and GBG for the GBG dataset. Default is GBG.")
 
-args = parser.parse_args()
 
 def main() -> None:
+    parser = argparse.ArgumentParser(description="Process file information.")
+    parser.add_argument("--sessionName", "-s", dest="session", type=str, default="GPT-3.5-REST-at-BTHS-eval", help="Customize the session name")
+    parser.add_argument("--model", "-m", dest="model", type=str, default="gpt3.5", help="Set the model to use. Choose between GPT-3.5 and GPT-4. Default is GPT-3.5.")
+    parser.add_argument("--data", "-d", dest="data", type=str, default="GBG", help="Customize the dataset, not case sensitive. Use MIX for the mix dataset, BTHS for the BTHS dataset, and GBG for the GBG dataset. Default is GBG.")
+
+    args = parser.parse_args()
+
     load_dotenv()
 
     session_name = args.session
-    model:str = ""
+    model: str = args.model.lower()
+    data: str = args.data.lower()
 
-    if args.model.lower() == "gpt3.5":
-        model = "gpt-3.5-turbo-0125"
-        print(f"Using {model}. Session name: {session_name}")
-    else: 
+    if model == "gpt4":
         model = "gpt-4-turbo-2024-04-09"
         print(f"Using Mistral model. Session name: {session_name}")
+    else:
+        model = "gpt-3.5-turbo-0125"
+        print(f"Using {model}. Session name: {session_name}")
 
-    req_path = ""
-    test_path = ""
+    req_path: str
+    test_path: str
     
-    if args.data.lower() == "mix":
+    if data == "mix":
         print("Using MIX data")
         req_path = os.getenv("MIX_REQ_PATH"),
         test_path = os.getenv("MIX_TEST_PATH")
-    elif args.data.lower() == "bths":
+    elif data == "bths":
         print("Using BTHS data")
         req_path = os.getenv("BTHS_REQ_PATH"),
         test_path = os.getenv("BTHS_TEST_PATH")
@@ -42,9 +45,6 @@ def main() -> None:
         print("Using GBG data")
         req_path = os.getenv("GBG_REQ_PATH"),
         test_path = os.getenv("GBG_TEST_PATH")
-        return
-
-
 
     # Load the REST specifications
     specs: RESTSpecification = RESTSpecification.load_specs(
